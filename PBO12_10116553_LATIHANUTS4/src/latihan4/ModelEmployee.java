dd6d00ef4c4651ee9e99ff55a817482fae8b7b74
ferialrpt <32388093+ferialrpt@users.noreply.github.com>
2017-11-14 16:35:47 +0700

    Add files via upload

---
+++ PBO12_10116553_LATIHANUTS4/src/latihan4/ModelEmployee.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan4;
+
+/**
+ *
+ * @author MY PC
+ */
+public class ModelEmployee {
+
+    protected float salary;
+
+    public ModelEmployee(){
+        this.salary=40000;
+    }
+    
+}
